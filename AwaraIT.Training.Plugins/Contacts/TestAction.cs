dabce1dd1495ee7922b41b2e3bda95c8240d5ae7
Alexander Vinokurov <Alexander.Vinokurov@awara-it.com>
2024-11-03 18:46:35 +0400

    feat: added input and output params for test action

--- AwaraIT.Training.Plugins/Contacts/TestAction.cs
+++ AwaraIT.Training.Plugins/Contacts/TestAction.cs
@@ -1,4 +1,5 @@
 ﻿using AwaraIT.Training.Plugins.PluginExtensions;
+using Microsoft.Xrm.Sdk.Workflow;
 using System;
 using System.Activities;
 
@@ -6,17 +7,27 @@ namespace AwaraIT.Training.Plugins.Contacts
 {
     public class TestAction : BasicActivity
     {
+        [Input("InputTest")]
+        public InArgument<string> InputTest { get; set; }
+
+        [Output("ErrorMessage")]
+        public OutArgument<string> ErrorMessage { get; set; }
+
         protected override void Execute(CodeActivityContext executionContext)
         {
             base.Execute(executionContext);
 
+            var inputTest = InputTest.Get(executionContext);
+            ErrorMessage.Set(executionContext, "");
+
             try
             {
-                Logger.INFO("Action TestAction", $"Ok: {DateTime.UtcNow}");
+                Logger.INFO("Action TestAction", $"Ok: {DateTime.UtcNow}. {inputTest}");
             }
             catch (Exception ex)
             {
                 Logger.ERROR("Action TestAction", ex.ToString());
+                ErrorMessage.Set(executionContext, ex.Message);
             }
         }
     }
