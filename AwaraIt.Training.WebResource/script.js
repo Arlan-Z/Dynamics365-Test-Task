4dc4bef22bba8d58ac5b5e861796998065edae4b
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-11-29 03:39:29 +0500

    action added

--- AwaraIt.Training.WebResource/script.js
+++ AwaraIt.Training.WebResource/script.js
@@ -6,39 +6,41 @@ function callActionOnFieldChange(executionContext) {
     var discount = formContext.getAttribute("arl_discpount").getValue() || 0;
     var discountPercent = formContext.getAttribute("arl_discountpercent").getValue() || 0;
 
-    if (possibleDeal && product) {
-        var actionName = "ShopCartAction";
-        var inputParams = {
-            "Deal": {
-                "@odata.type": "Microsoft.Dynamics.CRM.EntityReference",
-                "id": possibleDeal[0].id,
-                "logicalName": "arl_possibledealid"
-            },
-            "Product": {
-                "@odata.type": "Microsoft.Dynamics.CRM.EntityReference",
-                "id": product[0].id,
-                "logicalName": "arl_productsid"
-            },
-            "Discount": discount,
-            "DiscountPercent": discountPercent,
-        };
-
-        Xrm.WebApi.execute({
-            entityName: actionName,
-            entityType: "Microsoft.Dynamics.CRM.Action",
-            parameters: inputParams
-        }).then(
-            function success(result) {
-                var basePrice = result.response.OutputParameters.BasePrice;
-                var priceAfterDiscount = result.response.OutputParameters.PriceAfterDiscount;
-
-                formContext.getAttribute("arl_basePrice").setValue(basePrice);
-                formContext.getAttribute("arl_totalPrice").setValue(priceAfterDiscount);
-            },
-            function error(error) {
-                console.error(error.message);
-                alert("Ошибка при вызове Action: " + error.message);
-            }
-        );
-    }
+    var execute_arl_ShopCartAction_Request = {
+        // Parameters
+        DiscountPercent: discountPercent, // Edm.Decimal
+        Discount: discount, // Edm.Double
+        PossibleDeal: { "@odata.type": "Microsoft.Dynamics.CRM.arl_possibledeal", arl_possibledealid :  possibleDeal[0].id}, // mscrm.arl_possibledeal
+        ProductId: { "@odata.type": "Microsoft.Dynamics.CRM.arl_products", arl_productsid : product[0].id }, // mscrm.arl_products
+    
+        getMetadata: function () {
+            return {
+                boundParameter: null,
+                parameterTypes: {
+                    DiscountPercent: { typeName: "Edm.Decimal", structuralProperty: 1 },
+                    Discount: { typeName: "Edm.Double", structuralProperty: 1 },
+                    PossibleDeal: { typeName: "mscrm.arl_possibledeal", structuralProperty: 5 },
+                    ProductId: { typeName: "mscrm.arl_products", structuralProperty: 5 }
+                },
+                operationType: 0, operationName: "arl_ShopCartAction"
+            };
+        }
+    };
+    
+    Xrm.WebApi.execute(execute_arl_ShopCartAction_Request).then(
+        function success(response) {
+            if (response.ok) { return response.json(); }
+        }
+    ).then(function (responseBody) {
+        var result = responseBody;
+        console.log(result);
+        // Return Type: mscrm.arl_ShopCartActionResponse
+        // Output Parameters
+        var baseprice = result["BasePrice"]; // Edm.Double
+        var totalprice = result["TotalPrice"]; // Edm.Double
+        formContext.getAttribute("arl_baseprice").setValue(baseprice);
+        formContext.getAttribute("arl_totalprice").setValue(totalprice);
+    }).catch(function (error) {
+        console.log(error.message);
+    });
 }
